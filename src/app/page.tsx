c397dcf9b2a1d6b940395464f2cc85ba5207a791
Mikael <dev.kov.mtl@gmail.com>
2024-03-06 14:31:07 -0500

    Home page clerck button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,29 @@
+'use client';
+
 import { Button } from '@/components/ui/button';
-import { SignInButton } from '@clerk/nextjs';
+import {
+  SignInButton,
+  SignOutButton,
+  SignedIn,
+  SignedOut,
+  useSession,
+} from '@clerk/nextjs';
+
 export default function Home() {
+  const session = useSession();
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button variant={'outline'} size={'default'}>
-        ShadcnUI
-      </Button>
-
-      <SignInButton mode="modal">Sign in</SignInButton>
+      <SignedIn>
+        <SignOutButton>
+          <Button>Sign Out</Button>
+        </SignOutButton>
+      </SignedIn>
+      <SignedOut>
+        <SignInButton mode="modal">
+          <Button>Sign In</Button>
+        </SignInButton>
+      </SignedOut>
     </main>
   );
 }
