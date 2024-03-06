da106cde1d1949a9e4057f58e0e493f1cd5c7c23
Mikael <dev.kov.mtl@gmail.com>
2024-03-06 16:00:41 -0500

    auth in convex

--- convex/files.ts
+++ convex/files.ts
@@ -1,4 +1,4 @@
-import { v } from 'convex/values';
+import { ConvexError, v } from 'convex/values';
 import { mutation, query } from './_generated/server';
 
 // mutation: enpoint to create a new file
@@ -7,13 +7,25 @@ export const createFile = mutation({
     name: v.string(),
   },
   async handler(ctx, args) {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      throw new ConvexError('You must be signed in to create a file');
+    }
+
     await ctx.db.insert('files', { name: args.name });
   },
 });
 
 export const getFiles = query({
   args: {},
-  handler(ctx, args) {
+  async handler(ctx, args) {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      return [];
+    }
+
     // return every entry stored in the 'files' collection
     return ctx.db.query('files').collect();
   },
