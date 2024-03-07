875e8b45cdce10739897639bd5ca5309c5e1f082
Mikael <dev.kov.mtl@gmail.com>
2024-03-07 11:36:34 -0500

    Update header

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -1,13 +1,37 @@
-import { OrganizationSwitcher, UserButton } from '@clerk/nextjs';
+import Link from 'next/link';
+import Image from 'next/image';
+import {
+  OrganizationSwitcher,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs';
+import { Button } from '@/components/ui/button';
 
 export default function Header() {
   return (
-    <div className="border-b py-4 bg-gray-50">
-      <div className="container flex mx-auto justify-between items-center">
-        <div>FileDrive</div>
+    <div className="relative z-10 border-b py-4 bg-gray-50">
+      <div className="items-center container mx-auto justify-between flex">
+        <Link href="/" className="flex gap-2 items-center text-xl text-black">
+          <Image src="/logo.png" width="50" height="50" alt="file drive logo" />
+          FileDrive
+        </Link>
+
+        <SignedIn>
+          <Button variant={'outline'}>
+            <Link href="/dashboard/files">Your Files</Link>
+          </Button>
+        </SignedIn>
+
         <div className="flex gap-2">
           <OrganizationSwitcher />
           <UserButton />
+          <SignedOut>
+            <SignInButton>
+              <Button>Sign In</Button>
+            </SignInButton>
+          </SignedOut>
         </div>
       </div>
     </div>
