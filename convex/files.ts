f756a4bd70d7df5db2f6016213bb32c9e54f4b52
Mikael <dev.kov.mtl@gmail.com>
2024-03-08 14:03:18 -0500

    orgId Authorization

--- convex/files.ts
+++ convex/files.ts
@@ -1,5 +1,38 @@
 import { ConvexError, v } from 'convex/values';
-import { mutation, query } from './_generated/server';
+import { MutationCtx, QueryCtx, mutation, query } from './_generated/server';
+import { getUser } from './users';
+
+export async function hasAccessToOrg(
+  ctx: QueryCtx | MutationCtx,
+  orgId: string
+) {
+  const identity = await ctx.auth.getUserIdentity();
+
+  if (!identity) {
+    return null;
+  }
+
+  const user = await ctx.db
+    .query('users')
+    .withIndex('by_tokenIdentifier', (q) =>
+      q.eq('tokenIdentifier', identity.tokenIdentifier)
+    )
+    .first();
+
+  if (!user) {
+    return null;
+  }
+
+  const hasAccess =
+    user.orgIds.some((item) => item === orgId) ||
+    user.tokenIdentifier.includes(orgId);
+
+  if (!hasAccess) {
+    return null;
+  }
+
+  return { user };
+}
 
 // mutation: enpoint to create a new file
 export const createFile = mutation({
@@ -14,6 +47,25 @@ export const createFile = mutation({
       throw new ConvexError('You must be signed in to create a file');
     }
 
+    // const user = await ctx.db
+    //   .query('users')
+    //   .withIndex('by_todwsakenIdentifier', (q) =>
+    //     q.eq('tokenIdentifier', identity.tokenIdentifier)
+    //   )
+    //   .first();
+    // const user = await getUser(ctx, identity.tokenIdentifier);
+
+    // const hasAccess =
+    //   user.orgIds.some((item) => item === args.orgId) ||
+    //   user.tokenIdentifier.includes(args.orgId);
+
+    // console.log(hasAccess);
+    const hasAccess = await hasAccessToOrg(ctx, args.orgId);
+
+    if (!hasAccess) {
+      throw new ConvexError('You do not have permission to create a file');
+    }
+
     await ctx.db.insert('files', { name: args.name, orgId: args.orgId });
   },
 });
@@ -26,6 +78,14 @@ export const getFiles = query({
     const identity = await ctx.auth.getUserIdentity();
 
     if (!identity) {
+      return [];
+    }
+
+    const hasAccess = await hasAccessToOrg(ctx, args.orgId);
+
+    console.log(hasAccess);
+
+    if (!hasAccess) {
       return [];
     }
 
