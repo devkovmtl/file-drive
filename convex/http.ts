eb73b4e88f29ef3da0726009023ae1069c6cb916
Mikael <dev.kov.mtl@gmail.com>
2024-03-11 13:13:00 -0400

    http convex update

--- convex/http.ts
+++ convex/http.ts
@@ -32,10 +32,21 @@ http.route({
             image: result.data.image_url,
           });
           break;
+
+        case 'user.updated':
+          await ctx.runMutation(internal.users.updateUser, {
+            tokenIdentifier: `${process.env.CLERK_HOSTNAME}|${result.data.id}`,
+            name: `${result.data.first_name ?? ''} ${
+              result.data.last_name ?? ''
+            }`,
+            image: result.data.image_url,
+          });
+
         case 'organizationMembership.created':
           await ctx.runMutation(internal.users.addOrgIdsToUser, {
             tokenIdentifier: `${process.env.CLERK_HOSTNAME}|${result.data.public_user_data.user_id}`,
             orgId: result.data.organization.id,
+            role: result.data.role === 'admin' ? 'admin' : 'member',
           });
           break;
       }
