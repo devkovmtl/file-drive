12c442c148e340ba60cb5e9c3a97a24c70766b4b
Mikael <dev.kov.mtl@gmail.com>
2024-03-12 07:56:08 -0400

    http organization update

--- convex/http.ts
+++ convex/http.ts
@@ -50,6 +50,14 @@ http.route({
             role: result.data.role === 'org:admin' ? 'admin' : 'member',
           });
           break;
+
+        case 'organizationMembership.updated':
+          await ctx.runMutation(internal.users.updateRoleInOrgForUser, {
+            tokenIdentifier: `${process.env.CLERK_HOSTNAME}|${result.data.public_user_data.user_id}`,
+            orgId: result.data.organization.id,
+            role: result.data.role === 'org:admin' ? 'admin' : 'member',
+          });
+          break;
       }
 
       return new Response(null, {
