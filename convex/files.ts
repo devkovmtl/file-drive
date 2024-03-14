f0eeb91e4ae75a6f26a4d8d6b66c4077ff18aa4b
Mikael <dev.kov.mtl@gmail.com>
2024-03-14 13:40:17 -0400

    files convex get favorites api

--- convex/files.ts
+++ convex/files.ts
@@ -239,3 +239,56 @@ async function hasAccessToFile(
 
   return { user: hasAccess.user, file };
 }
+
+export const toggleFavorite = mutation({
+  args: { fileId: v.id('files') },
+  async handler(ctx, args) {
+    const access = await hasAccessToFile(ctx, args.fileId);
+
+    if (!access) {
+      throw new ConvexError('You do not have permission to favorite this file');
+    }
+
+    const favorite = await ctx.db
+      .query('favorites')
+      .withIndex('by_userId_orgId_fileId', (q) =>
+        q
+          .eq('userId', access.user._id)
+          .eq('orgId', access.file.orgId!)
+          .eq('fileId', args.fileId)
+      )
+      .first();
+
+    if (!favorite) {
+      await ctx.db.insert('favorites', {
+        userId: access.user._id,
+        orgId: access.file.orgId!,
+        fileId: access.file._id,
+      });
+    } else {
+      await ctx.db.delete(favorite._id);
+    }
+  },
+});
+
+export const getAllFavorites = query({
+  args: {
+    orgId: v.string(),
+  },
+  handler: async (ctx, args) => {
+    const hasAccess = await hasAccessToOrg(ctx, args.orgId);
+
+    if (!hasAccess) {
+      return [];
+    }
+
+    const favorites = await ctx.db
+      .query('favorites')
+      .withIndex('by_userId_orgId_fileId', (q) =>
+        q.eq('userId', hasAccess.user._id).eq('orgId', args.orgId)
+      )
+      .collect();
+
+    return favorites;
+  },
+});
