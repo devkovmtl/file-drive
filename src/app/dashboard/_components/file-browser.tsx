a6cfc5b770bb5f69456e2574deaf0a9740434480
Mikael <dev.kov.mtl@gmail.com>
2024-03-18 11:29:35 -0400

    file browser with table

--- src/app/dashboard/_components/file-browser.tsx
+++ src/app/dashboard/_components/file-browser.tsx
@@ -11,6 +11,20 @@ import { api } from '../../../../convex/_generated/api';
 import UploadButton from './upload-button';
 import SearchBar from './search-bar';
 
+import { columns } from './columns';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
+import { GridIcon, Loader2, RowsIcon } from 'lucide-react';
+import { Label } from '@/components/ui/label';
+import FileCard from './file-card';
+import DataTable from './file-table';
+
 function Placeholder() {
   return (
     <div className="flex flex-col gap-8 items-center mt-12">
@@ -65,7 +79,7 @@ export default function FileBrowser({
 
   const isLoading = files === undefined;
 
-  const mofdifiedFiles =
+  const modifiedFiles =
     files?.map((file) => ({
       ...file,
       isFavorited: (favorites ?? []).some(
@@ -82,6 +96,58 @@ export default function FileBrowser({
 
         <UploadButton />
       </div>
+
+      <Tabs defaultValue="grid">
+        <div className="flex justify-between items-center">
+          <TabsList className="mb-2">
+            <TabsTrigger value="grid" className="flex gap-2 items-center">
+              <GridIcon />
+              Grid
+            </TabsTrigger>
+            <TabsTrigger value="table" className="flex gap-2 items-center">
+              <RowsIcon /> Table
+            </TabsTrigger>
+          </TabsList>
+
+          <div className="flex gap-2 items-center">
+            <Label htmlFor="type-select">Type Filter</Label>
+            <Select
+              value={type}
+              onValueChange={(newType: any) => {
+                setType(newType as any);
+              }}
+            >
+              <SelectTrigger id="type-select" className="w-[180px]">
+                <SelectValue />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="all">All</SelectItem>
+                <SelectItem value="image">Image</SelectItem>
+                <SelectItem value="csv">CSV</SelectItem>
+                <SelectItem value="pdf">PDF</SelectItem>
+              </SelectContent>
+            </Select>
+          </div>
+        </div>
+
+        {isLoading && (
+          <div className="flex flex-col gap-8 w-full items-center mt-24">
+            <Loader2 className="h-32 w-32 animate-spin text-gray-500" />
+            <div className="text-2xl">Loading your files...</div>
+          </div>
+        )}
+
+        <TabsContent value="grid">
+          <div className="grid grid-cols-3 gap-4">
+            {modifiedFiles?.map((file) => {
+              return <FileCard key={file._id} file={file} isFavorited />;
+            })}
+          </div>
+        </TabsContent>
+        <TabsContent value="table">
+          <DataTable columns={columns} data={modifiedFiles} />
+        </TabsContent>
+      </Tabs>
 
       {files?.length === 0 && <Placeholder />}
     </div>
